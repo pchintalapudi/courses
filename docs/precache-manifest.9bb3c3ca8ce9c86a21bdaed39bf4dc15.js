c54ba04633d8a10825d237ba12ddcafafaf47b81
Prem Chintalapudi <prem.chintalapudi@gmail.com>
2020-01-18 20:49:32 -0500

    build

---
+++ docs/precache-manifest.9bb3c3ca8ce9c86a21bdaed39bf4dc15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05194f1caf320b0eae27",
+    "url": "/css/app.a90b86a3.css"
+  },
+  {
+    "revision": "73882fd2146d00afde3c70e199848105",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05194f1caf320b0eae27",
+    "url": "/js/app.673aeb27.js"
+  },
+  {
+    "revision": "c9733f972a486ae85f92",
+    "url": "/js/chunk-vendors.fb3192a9.js"
+  },
+  {
+    "revision": "c3097ccf623a47e53bc909acba482bc7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
